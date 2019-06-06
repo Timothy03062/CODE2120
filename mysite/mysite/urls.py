9836957a9d190b5b79545049025eab9d6cfc1521
Timothy03062 <36873809+Timothy03062@users.noreply.github.com>
2019-06-06 13:14:49 +1000

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path, re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'mantra/', include('mantra.urls')),
-    path(r'example/', include('example.urls')), 
+    path(r'example/', include('example.urls')),
+    #Whatever 
 ]
